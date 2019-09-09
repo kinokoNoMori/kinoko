15437b80191ce13c0e24e3e843f764d86a4cb556
kinokoNoMori <ftymdk@yahoo.co.jp>
2019-09-09 05:04:56 +0000

    aa

---
+++ tomcat/webapps/mydatabase/SelectSqlServlet.java
@@ -0,0 +1,49 @@
+// JDBCで、データーベース呼び出し
+// Servlet単独
+
+import java.io.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.sql.*;
+
+public class SelectSqlServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+        response.setContentType("text/html");
+        response.setCharacterEncoding("utf-8");
+        PrintWriter out = response.getWriter();
+
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Java - paiza</title>");
+        out.println("</head>");
+        out.println("<body>");
+
+        out.println("<p>Hello World!</p>");
+
+        String url = "jdbc:mysql://localhost/mydb";
+        String user = "root";
+        String password = "";
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try (Connection connection = DriverManager.getConnection(url, user, password);
+             PreparedStatement statment = connection.prepareStatement("select * from players");
+             ResultSet results = statment.executeQuery()) {
+
+            out.println("<p>接続成功！</p>");
+
+        } catch (Exception e) {
+            out.println("Exception" + e.getMessage());
+        }
+        out.println("<p>Hello World!</p>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
