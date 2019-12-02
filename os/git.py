deda67379d38104727e9cd234e2c4952455fa7c6
KinokoNoMori <ftymdk@yahoo.co.jp>
2019-12-02 01:21:34 +0000

    commit test

---
+++ os/git.py
@@ -0,0 +1,33 @@
+import os, time
+
+# tomcat起動
+def git_pull(mess):
+    print("ホームディレクトリを移動します。")
+    os.chdir("/home/ubuntu/kinoko")
+    time.sleep(1)
+    print("pullします。")
+    os.system("git pull")#pullもできる
+    time.sleep(3)
+    os.system("git add .")#pullもできる
+    time.sleep(3)
+    os.system("git commit -m \""+ mess + "\"")#pullもできる
+    
+#メインメソッド
+def main():
+    git_pull("commit test")
+
+
+
+
+
+# ==============
+
+#実行
+main()
+#os.chdir("/home/ubuntu/kinoko/os")
+#os.system("python3 tom_boot.py")これでpython3ファイルが実行できるな
+
+# ==============
+#参考 https://teratail.com/questions/183558
+
+#現在地の出力 print(os.getcwd())
